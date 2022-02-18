8b75e738a6ff21171dcd29269dccbccd2f9eec3c
Naadir Jeewa <naadir@saana.home.internal.randomvariable.co.uk>
2022-02-18 12:21:25 +0000

    Make compatible with wireshark

--- mitm.sh
+++ mitm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 docker exec -it "$1" /bin/mkdir -p /etc/systemd/system/containerd.service.d
 docker cp 10-proxy.conf "$1:/etc/systemd/system/containerd.service.d/10-proxy.conf"
 docker cp config.toml "$1:/etc/containerd/config.toml"
 docker exec -it "$1" /bin/systemctl daemon-reload
 docker exec -it "$1" /bin/systemctl restart containerd
+
+mitmproxy -k --set tls_version_server_max=TLS1_2 --set tls_version_client_max=TLS1_2 --set ciphers_server=RSA
